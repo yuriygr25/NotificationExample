7323fff2e96d6dd3f2b720d0e15078ecb4751c51
Yuriy Ivanov <yuriygr@yahoo.com>
2018-10-18 01:59:33 +0300

    commit

---
+++ app/src/main/java/com/example/yura/notificationexample/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.yura.notificationexample;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Идентификатор уведомления
+    private static final int NOTIFY_ID = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClick(View view) {
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, notificationIntent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+
+        Resources res = this.getResources();
+
+        // до версии Android 8.0 API 26
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+        builder.setContentIntent(contentIntent)
+                // обязательные настройки
+                .setSmallIcon(R.drawable.hungrycat)
+                //.setContentTitle(res.getString(R.string.notifytitle)) // Заголовок уведомления
+                .setContentTitle("Напоминание")
+                //.setContentText(res.getString(R.string.notifytext))
+                .setContentText("Пора покормить кота") // Текст уведомления
+                // необязательные настройки
+                .setLargeIcon(BitmapFactory.decodeResource(res, R.drawable.hungrycat)) // большая
+                // картинка
+                //.setTicker(res.getString(R.string.warning)) // текст в строке состояния
+                .setTicker("Последнее китайское предупреждение!")
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true); // автоматически закрыть уведомление после нажатия
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // Альтернативный вариант
+        // NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(NOTIFY_ID, builder.build());
+    }
+}
